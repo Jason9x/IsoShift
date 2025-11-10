488f9cbe3e668d43da2047536adf6f1ab0593af3
Jason Olimpio <olimpiojason@gmail.com>
2025-11-10 03:21:51 +0100

    Updates

---
+++ assets/viewport-BYp2gMPk.js
@@ -0,0 +1,2 @@
+import{z as e,E as r}from"./index-BUTSG_17.js";let l=null;const m=o=>{l=o,e.value=o.scale.x,o.on("zoomed",()=>e.value=o.scale.x),r(()=>{l?.setZoom(e.value,!0)})};export{m as setViewport};
+//# sourceMappingURL=viewport-BYp2gMPk.js.map
--- assets/viewport-Do6HE2oo.js
+++
@@ -1,2 +0,0 @@
-import{z as e,E as r}from"./index-C9Mk3x7h.js";let l=null;const m=o=>{l=o,e.value=o.scale.x,o.on("zoomed",()=>e.value=o.scale.x),r(()=>{l?.setZoom(e.value,!0)})};export{m as setViewport};
-//# sourceMappingURL=viewport-Do6HE2oo.js.map
